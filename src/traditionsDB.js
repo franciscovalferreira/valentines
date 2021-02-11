16e34341ef0caa5c3bb591dce7fba27c2725e205
Francisco Ferreira <franciscovalferreira@gmail.com>
2021-02-11 00:23:39 +0000

    Added all traditions

--- src/traditionsDB.js
+++ src/traditionsDB.js
@@ -1,6 +1,6 @@
 var traditions = [
     {
-        tradition:"Salta à fogueira de Santo António três vezes, mas em direções diferentes, e deixa cair uma moeda na fogueira ao saltar pela última vez. No dia seguinte, antes do romper do sol, vai buscá-la e a dá-a ao primeiro pobre. O noivo vai a ser de nome igual ao do pobre.",
+        tradition:"Salta à fogueira de Santo António três vezes, mas em direções diferentes, e deixa cair uma moeda na fogueira ao saltar pela última vez. No dia seguinte, antes do romper do sol, vai buscá-la e dá-a ao primeiro pobre. O noivo vai ser de nome igual ao do pobre.",
         source:"Santo António na Tradição Popular, Armando de Mattos, 1937, p.67"
     },
     {
@@ -15,6 +15,130 @@ var traditions = [
         tradition:"Para que conheça o nome da rapariga, o rapaz deve cortar botões de rosa e dar nomes de diversas raparigas de quem gosta. Na manhã seguinte à da noite do santo, irá vê-los. O que estiver mais aberto é o que indica o nome da noiva.",
         source:"Santo António na Tradição Popular, Armando de Mattos, 1937, p. 74"
     },
+    {
+        tradition:"Deita uma clara de ovo num copo de água e conforme os feitios que tomar assim será a profissão do noivo (marinheiro, se a clara de ovo se assemelhar a um navio, etc.).",
+        source:"[Em Lisboa, na noite de Santo António], Etnografia Portuguesa, Vol. VII, J. Leite de Vasconcelos, Lisboa, edição fac-similada de 1980, p.426"
+    },
+    {
+        tradition:"Escreve o nome dos pretendentes em papelinhos, enrola-os e deita-os num copo de água fresca e, por fim, benze rezando o credo em cruz. Na manhã seguinte, o papel que estiver desenrolado vai indicar o nome ambicionado.",
+        source:"Santo António na Tradição Popular, Armando de Mattos, 1937, p. 73"
+    },
+    {
+        tradition:"Na noite de Santo António deita um cravo à rua: se este for apanhado por pessoa de idade, faltam poucos anos para casar; mas se for por um jovem, faltam muitos.",
+        source:"[Lisboa] Etnografia Portuguesa, Vol. VII, J. Leite de Vasconcelos, Lisboa, edição fac-similada de 1980, p. 288"
+    },
+    {
+        tradition:"Aperta uma laçada de cordel à volta do pescoço de Santo António até que surja namorado/a.",
+        source:"Alfacinhas, Alfredo de Mesquita, Lisboa, 1910, p. 122"
+    },
+    {
+        tradition:"No dia de Santo António, dá três voltas à sua igreja; ao fim de um ano estarás casado/a.",
+        source:"Etnografia Portuguesa, Vol. VIII, J. Leite de Vasconcelos, Lisboa, edição fac-similada de 1982, p. 375"
+    },
+    {
+        tradition:"Deita um cravo para o meio da rua ao meio-dia, e espreita: se for um homem a apanhá-lo, o nome dele será o do futuro noivo; se for mulher, é presságio de que nunca se vai casar.",
+        source:"[Óbidos] Etnografia Portuguesa, Vol. VIII, J. Leite de Vasconcelos, Lisboa, edição fac-similada de 1982, p. 368"
+    },
+    {
+        tradition:"Tira o cordão ao Santo. Este só será restituído depois de encontrar o amor.",
+        source:"[Na Igreja dos Olivais, em Coimbra], Santo António na Tradição Popular, Armando de Mattos, 1937, p. 82"
+    },
+    {
+        tradition:"Vira a imagem de Santo António para a parede até arranjar namorado.",
+        source:"Etnografia Portuguesa, Vol. IX, J. Leite de Vasconcelos, Lisboa, edição fac-similada de 1985"
+    },
+    {
+        tradition:"Vai à ermida de Santo António do Alto e entra no adro arqueado, fecha os olhos ao entrar e, de dedo indicador estendido, procura acertar no buraco da fechadura da porta; se o conseguires casarás brevemente.",
+        source:"[Em Faro], Etnografia Portuguesa, Vol. X, J. Leite de Vasconcelos, Lisboa, edição fac-similada de 1988, p. 193"
+    },
+    {
+        tradition:"Se queres casar, atira ao chão três vezes um Santo António ou um Menino Jesus, ora um ora outro, na própria casa (mas sem o partir).",
+        source:"[Peral, concelho do Cadaval] Etnografia Portuguesa, Vol. X, J. Leite de Vasconcelos, Lisboa, edição fac-similada de 1988, p. 283"
+    },
+    {
+        tradition:"Queima uma alcachofra na fogueira de Santo António para saber se casas este ano, o que se pronuncia se a alcachofra reverdece depois de chamuscada.",
+        source:"Santo António na Tradição Popular, Armando de Mattos, 1937, p. 83"
+    },
+    {
+        tradition:"Para arranjares namorado/a, tira o resplendor ao Santo e coloca sobre a tonsura uma moeda pregada com cera.",
+        source:"Etnografia Portuguesa, Vol. VIII, J. Leite de Vasconcelos, Lisboa, edição fac-similada de 1982, p. 373"
+    },
+    {
+        tradition:"Descalça um sapato e deita-o por uma escada, e quantos degraus faltarem para o sapato chegar ao chão, são tantos os anos que faltam para casar.",
+        source:"Estudo Biográfico de Santo António – História dos seus milagres, Guilherme Rodrigues, Lisboa, 1895"
+    },
+    {
+        tradition:"Bochecha água e deita-a à rua à meia-noite. A primeira pessoa do sexo oposto a passar terá o nome do noivo ou da noiva.",
+        source:"Estudo Biográfico de Santo António – História dos seus milagres, Guilherme Rodrigues, Lisboa, 1895"
+    },
+    {
+        tradition:"Pendura a imagem do santo por um cordel e mergulha-a num poço, quando quiseres que o santo te arranje o amor pretendido.",
+        source:"Estudo Biográfico de Santo António – História dos seus milagres, Guilherme Rodrigues, Lisboa, 1895"
+    },
+    {
+        tradition:"Lança uma moeda à estátua de Santo António em Lisboa, mas a moeda tem de ficar no livro aberto que o Santo tem na mão.",
+        source:"Tradição lisboeta"
+    },
+    {
+        tradition:"Todo o Santo António tem um menino Jesus nos braços. Tire-o e esconda-o em algum canto da casa. Só pode devolver quando o pedido já estiver realizado.",
+        source:"Tradição oral Brasileira"
+    },
+    {
+        tradition:"Compre uma imagem de Santo António e no dia 13 de junho separe o Menino Jesus e peça: “Santo António, Santo Antoninho, faça *tal pessoa* casar-se comigo, que eu devolvo o teu Menino”. Mantenha o Menino Jesus separado do Santo e só devolva quando casar.",
+        source:"Tradição oral Brasileira"
+    },
+    {
+        tradition:"Guarde a imagem de Santo António dentro da geladeira. Fale para a imagem do Santo que enquanto ele não te arrumar um namorado, ficará na geladeira. E, que se demorar, irá para o congelador. Assim que seu amor bater à porta, não esqueça de tirá-lo de lá e agradecer.",
+        source:"Tradição oral Brasileira"
+    },
+    {
+        tradition:"Escreva em três pedaços de papel o nome de alguns pretendentes. Dentro da igreja, numa caminhada em direção ao altar, dois deles devem ser atirados no chão. Beije o nome daquele que restou e guarde debaixo dos pés do santo.",
+        source:"Tradição oral Brasileira"
+    },
+    {
+        tradition:"Amarre o Santo com sete fitas coloridas e liberte-o só quando o casamento acontecer.",
+        source:"Tradição oral Brasileira"
+    },
+    {
+        tradition:"Vire a imagem de Santo António de cabeça para baixo dentro de um copo de água dizendo que o colocará de pé quando arranjar um namorado.",
+        source:"Tradição oral Brasileira"
+    },
+    {
+        tradition:"Coloque uma jarra com sete rosas perto da imagem. A cada mês, tire uma rosa e molhe o corpo do santo. No fim do sétimo mês, o marido vai aparecer.",
+        source:"Tradição oral Brasileira"
+    },
+    {
+        tradition:"Para quem já tem namorado mas quer oficializar a relação, amarre um fio de cabelo de cada um dos pombinhos no corpo do santo. Ele irá agilizar os procedimentos e rapidamente virá o pedido de casamento.",
+        source:"Tradição oral Brasileira"
+    },
+    {
+        tradition:"Escreva o nome dos candidatos em papéis separados; mas um dos papéis deve ser deixado em branco. À meia-noite do dia 12 de junho, eles devem ser colocados em cima de um prato com água, que passará a madrugada ao relento. No dia seguinte, o que estiver mais aberto indicará o escolhido.",
+        source:"Tradição oral Brasileira"
+    },
+    {
+        tradition:"Escreva com um palito numa vela, o seu nome e o da pessoa que quer conquistar. Passe mel na vela e depois acenda sobre um pires branco. É só esperar. Depois de alguns dias, essa pessoa especial virá até você.",
+        source:"Tradição oral Brasileira"
+    },
+    {
+        tradition:"Logo na manhã do dia 12 de junho, véspera de Santo António, compre um metro de fita azul, de qualquer largura, e escreva nela o nome completo da pessoa amada. Guarde a fita junto ao seu coração. À noite, conte 7 estrelas no céu, e enquanto conta faça um pedido ao santo, para que ele ajude a conquistar o coração dessa pessoa. No dia seguinte, amarre a fita nos pés da imagem de Santo António e deixe lá até conseguir namorar com ele ou com outro de seu agrado.",
+        source:"Tradição oral Brasileira"
+    },
+    {
+        tradition:"Pegue em 7 rosas e coloque-as num vaso bonito. Enquanto faz isso reze a Santo António. Depois delas secarem, leve as pétalas até uma igreja onde aconteçam muitos casamentos.",
+        source:"Tradição oral Brasileira"
+    },
+    {
+        tradition:"Amarre um fio do seu cabelo a um fio do cabelo do seu namorado. Esses 2 fios amarrados devem ser colocados aos pés do santo, que resolverá a questão.",
+        source:"Tradição oral Brasileira"
+    },
+    {
+        tradition:"Para descobrir se faltam muitos anos para casar, no dia 12 de Junho à meia-noite (na véspera do dia de Santo António) amarre uma aliança – que pode ser de qualquer parente – numa linha ou num fio. Coloque um copo sobre a mesa e segure o fio de modo que a aliança esteja dentro do copo. O fio com a aliança funcionará como um pêndulo. Pergunte então a Santo António quantos anos faltam para se casar. O número de batidas no copo informa quantos anos ainda faltam para o grande dia.",
+        source:"Tradição oral Brasileira"
+    },
+    {
+        tradition:"No dia de Santo António, a 13 de junho, coloque 2 agulhas iguaizinhas dentro de uma bacia com água, com 2 colheres de açúcar. No dia seguinte veja com estão as agulhas. Se estiverem juntas é porque o casamento está próximo.",
+        source:"Tradição oral Brasileira"
+    }
 ]
 
 export default traditions;
